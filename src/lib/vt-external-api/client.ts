3231c0def5785c8ed4a54afd21e283337b191dca
development@venueboost.io <development@venueboost.io>
2025-01-02 16:23:08 +0200

    feat: change https /

--- src/lib/vt-external-api/client.ts
+++ src/lib/vt-external-api/client.ts
@@ -1,7 +1,7 @@
 // lib/vt-external-api/client.ts
 import axios, { AxiosInstance, AxiosResponse } from 'axios';
 
-const VT_API_BASE_URL = 'http://coreapi.visiontrack.xyz';
+const VT_API_BASE_URL = 'https://coreapi.visiontrack.xyz';
 
 interface VTCredentials {
   platform_id: string;
