28ac7b4a0e738b4f33be0c1834c86d0aa65ce9c6
development@venueboost.io <development@venueboost.io>
2025-03-24 17:31:12 +0200

    fixed post upgrade  build issue

--- src/app/(site)/auth/invite/page.tsx
+++ src/app/(site)/auth/invite/page.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { Suspense } from "react";
 import InvitedSignin from "@/components/Auth/InvitedSignin";
 
 const InvitedSigninPage = () => {
 	return (
 		<main className='pt-[150px]'>
-			<InvitedSignin />
+			<Suspense fallback={<div>Loading...</div>}>
+				<InvitedSignin />
+			</Suspense>
 		</main>
 	);
 };
